c2da5b237e7830e6571ba6ba20e84fde7ad7fdea
amritkuikel <amritkuikel5689@gmail.com>
2025-07-01 12:40:33 +0545

    test

--- apps/frontend/src/components/shared/hero.tsx
+++ apps/frontend/src/components/shared/hero.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Hero = ({title,subtitle1,subtitle2,description,buttons}:{title?:string,description:string,subtitle1:string,subtitle2:string,buttons:boolean}) => {
+const Hero = ({
+  title,
+  subtitle1,
+  subtitle2,
+  description,
+  buttons,
+}: {
+  title?: string;
+  description: string;
+  subtitle1: string;
+  subtitle2: string;
+  buttons: boolean;
+}) => {
   return (
-    <div className='w-screen h-[796px] grid place-items-center'>
+    <div className="w-screen h-[796px] grid place-items-center">
       <div>{title}</div>
       <div>{subtitle1}</div>
       <div>{subtitle2}</div>
-
       <div>{description}</div>
-      {buttons &&   <div>
-        </div>}
-    
+      {buttons && <div></div>}
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
