c09606ca0a0cfadde7f7f8b78857fab4b3ec0d0a
amritkuikel <amritkuikel5689@gmail.com>
2025-07-01 12:01:41 +0545

    test

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -46,22 +46,22 @@ export default function Home() {
           <div>Body</div>
         </div>
       </div>
-      <div className="text-playfair-semibold-18">Playfair SemiBold 18: Hello Playfair SemiBold 18</div>
-      <div className="text-playfair-semibold-24">Playfair SemiBold 24: Hello Playfair SemiBold 24</div>
-      <div className="text-playfair-medium-22">Playfair Medium 22: Hello Playfair Medium 22</div>
-      <div className="text-playfair-medium-58">Playfair Medium 58: Hello Playfair Medium 58</div>
-      <div className="text-playfair-medium-62">Playfair Medium 62: Hello Playfair Medium 62</div>
-      <div className="text-playfair-extrabold-62 bg-linear  hover:bg-black  hover:[background-image:none]">Playfair ExtraBold 62: Hello Playfair ExtraBold 62</div>
-      <div className="text-playfair-bold-28">Playfair Bold 28: Hello Playfair Bold 28</div>
-      <div className="text-lato-regular-14  ">Lato Regular 14: Hello Lato Regular 14</div>
-      <div className="text-lato-regular-16">Lato Regular 16: Hello Lato Regular 16</div>
-      <div className="text-lato-regular-18">Lato Regular 18: Hello Lato Regular 18</div>
-      <div className="text-lato-regular-26">Lato Regular 26: Hello Lato Regular 26</div>
-      <div className="text-lato-regular-22">Lato Regular 22: Hello Lato Regular 22</div>
-      <div className="text-lato-semibold-16">Lato SemiBold 16: Hello Lato SemiBold 16</div>
-      <div className="text-great-vibes-regular-26">Great Vibes Regular 26: Hello Great Vibes Regular 26</div>
-      <div className="text-great-vibes-regular-32">Great Vibes Regular 32: Hello Great Vibes Regular 32</div>
-      <div className="text-cinzel-bold-34">Cinzel Bold 34: Hello Cinzel Bold 34</div>
+      <div className="playfair-semibold-18">Playfair SemiBold 18: Hello Playfair SemiBold 18</div>
+      <div className="playfair-semibold-24">Playfair SemiBold 24: Hello Playfair SemiBold 24</div>
+      <div className="playfair-medium-22">Playfair Medium 22: Hello Playfair Medium 22</div>
+      <div className="playfair-medium-58">Playfair Medium 58: Hello Playfair Medium 58</div>
+      <div className="playfair-medium-62">Playfair Medium 62: Hello Playfair Medium 62</div>
+      <div className="playfair-extrabold-62 bg-linear  hover:bg-black  hover:[background-image:none]">Playfair ExtraBold 62: Hello Playfair ExtraBold 62</div>
+      <div className="playfair-bold-28">Playfair Bold 28: Hello Playfair Bold 28</div>
+      <div className="lato-regular-14  ">Lato Regular 14: Hello Lato Regular 14</div>
+      <div className="lato-regular-16">Lato Regular 16: Hello Lato Regular 16</div>
+      <div className="lato-regular-18">Lato Regular 18: Hello Lato Regular 18</div>
+      <div className="lato-regular-26">Lato Regular 26: Hello Lato Regular 26</div>
+      <div className="lato-regular-22">Lato Regular 22: Hello Lato Regular 22</div>
+      <div className="lato-semibold-16">Lato SemiBold 16: Hello Lato SemiBold 16</div>
+      <div className="great-vibes-regular-26">Great Vibes Regular 26: Hello Great Vibes Regular 26</div>
+      <div className="great-vibes-regular-32">Great Vibes Regular 32: Hello Great Vibes Regular 32</div>
+      <div className="cinzel-bold-34">Cinzel Bold 34: Hello Cinzel Bold 34</div>
       <Navbar />
       <GradientText text="gradient text" className="playfair-extrabold-62"/>
     </div>
